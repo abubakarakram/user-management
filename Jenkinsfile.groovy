3c9de5da889e8577f510fcd8238ad320ec492b5d
brother <abubakarabubakar451@gmail.com>
2023-02-25 09:57:48 +0500

    jenkins groovy script

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,36 +1,39 @@
-pipeline {
-    agent any
-    options {
-        buildDiscarder(logRotator(numToKeepStr: '5'))
-    }
-    environment {
-        DOCKERHUB_CREDENTIALS = credentials('dockerhub')
-    }
-    stages {
-        stage('Build') {
-            steps {
-                sh 'docker build -t abubakar243/jenkins-docker-hub .'
-            }
+node{
+    pipeline {
+        agent any
+        options {
+            buildDiscarder(logRotator(numToKeepStr: '5'))
         }
-        stage('Login') {
-            steps {
-                withCredentials([string(credentialsId: 'dockerPassword', variable: 'dockerPassword')])
+        environment {
+            DOCKERHUB_CREDENTIALS = credentials('dockerhub')
+        }
+        stages {
+            stage('Build') {
+                steps {
+                    sh 'docker build -t abubakar243/jenkins-docker-hub .'
+                }
+            }
+            stage('Login') {
+                steps {
+                    withCredentials([string(credentialsId: 'dockerPassword', variable: 'dockerPassword')])
 
-                        {
-                            sh ' docker login -u abubakar243 -p ${dockerPassword}'
-                        }
+                            {
+                                sh ' docker login -u abubakar243 -p ${dockerPassword}'
+                            }
 
+                }
+            }
+            stage('Push') {
+                steps {
+                    sh 'docker push abubakar243/jenkins-docker-hub'
+                }
             }
         }
-        stage('Push') {
-            steps {
-                sh 'docker push abubakar243/jenkins-docker-hub'
+        post {
+            always {
+                sh 'docker logout'
             }
         }
     }
-    post {
-        always {
-            sh 'docker logout'
-        }
-    }
+
 }
