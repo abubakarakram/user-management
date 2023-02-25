ea577c434847e245536d3db7e512c23084a1ee3f
brother <abubakarabubakar451@gmail.com>
2023-02-26 00:48:51 +0500

    test

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -11,7 +11,7 @@ pipeline {
                 withCredentials([string(credentialsId: 'passwordId', variable: 'dockerPassword')]) {
 
 
-                        bat 'docker login -u abubakar243 - --password-stdin'
+                        bat 'docker login -u abubakar243 --password-stdin'
 
 
                 }
